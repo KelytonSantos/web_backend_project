45b6b7505959afb56d32367d11561c429874600c
Kelyton Santos <kelytonlucas@gmail.com>
2024-08-16 22:22:34 -0300

    Category

--- src/main/java/com/project/config/TestConfig.java
+++ src/main/java/com/project/config/TestConfig.java
@@ -11,7 +11,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.project.entities.Order;
 import com.project.entities.User;
+import com.project.entities.Category;
 import com.project.entities.enums.OrderStatus;
+import com.project.repositories.CategoryRepository;
 import com.project.repositories.OrderRepository;
 import com.project.repositories.UserRepository;
 
@@ -29,30 +31,37 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private EntityManager entityManager;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     @Transactional
     public void run(String... args) throws Exception {
 
-        // Excluir todos os registros
         orderRepository.deleteAll();
         userRepository.deleteAll();
+        categoryRepository.deleteAll();
 
-        // Redefinir o auto incremento para as tabelas User e Order
         resetSequence("tb_user_id_seq");
         resetSequence("tb_order_id_seq");
+        resetSequence("tb_category_id_seq");
 
-        // Criar e salvar novos usuários
         User u1 = new User(null, "Lucas", "lucas@santos.com", "99991929", "1234567");
         User u2 = new User(null, "Kelyton", "Kelyton@santos.com", "996269220", "123546");
 
         userRepository.saveAll(Arrays.asList(u1, u2));
 
-        // Criar e salvar novos pedidos
         Order o1 = new Order(null, Instant.parse("2024-08-16T19:53:07Z"), OrderStatus.PAID, u1);
         Order o2 = new Order(null, Instant.parse("2024-08-20T15:22:14Z"), OrderStatus.WAITING_PAYMENT, u2);
         Order o3 = new Order(null, Instant.parse("2024-08-27T09:04:37Z"), OrderStatus.WAITING_PAYMENT, u1);
 
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+        Category c1 = new Category(null, "Eletronics");
+        Category c2 = new Category(null, "Books");
+        Category c3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(c1, c2, c3));
     }
 
     private void resetSequence(String sequenceName) {
